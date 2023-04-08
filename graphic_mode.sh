2129a5ef5ccc0e7dc426f7ab0c0427d900596d34
maciejdudek92 <30751921+maciejdudek92@users.noreply.github.com>
2023-04-08 14:23:59 +0200

    Add files via upload

--- graphic_mode.sh
+++ graphic_mode.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-displayCount=$(xrandr | grep connected | wc -l)
+# screensCount=$(xrandr | grep connected | wc -l)
+vgaControllersCount=$(lspci | grep VGA | wc -l)
+# isSupergfdServiceDisabled=$(systemctl status supergfxd.service | grep inactive | wc -l) 
 
+if [ $vgaControllersCount == 1 ]; then
+	# if [ $isSupergfdServiceDisabled != 1 ]; then
+	# 	systemctl stop supergfxd.service
+	# fi
+	gnome-extensions disable supergfxctl-gex@asus-linux.org
 
-if [ $displayCount > 1 ]; then
-    supergfxctl --mode hybrid
-    echo "External Display was detected, setting graphic mode to HYBRID"
 else
-    supergfxctl --mode integrated
-    echo "No externaml Dieplay was detected. Nothing to do."
+	# if [ $isSupergfdServiceDisabled == 1 ]; then
+	# 	systemctl start supergfxd.service
+	# fi
+	gnome-extensions enable supergfxctl-gex@asus-linux.org
+	supergfxctl --mode integrated
 fi
+
+
